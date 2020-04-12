e7fd3787fcda63935869fd569c5559ecc84057df
canewsin <canews.in@gmail.com>
2020-04-12 16:11:23 +0530

    v 0.5.1
    - Removed Unsupported Platform x86 bins, these causes on few crashes on x86 phones, since only few binaries are available.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'common.dart';
 import 'widgets.dart';
 
+//TODO:Remainder: Removed Half baked x86 bins, add them when we support x86 platform
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await init();
